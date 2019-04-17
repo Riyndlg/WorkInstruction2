4613ddf7be1c9e43df51255849a3d33c640257be
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-17 19:27:17 +0300

    Practice case 1

---
+++ Practice case1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter how many seconds pass fron start of day: ");
+            int n = int.Parse(Console.ReadLine());
+            int hours = n / 3600;
+            Console.Write($"Hours pass from start of day: {hours}");
+            Console.ReadKey();
+        }
+    }
+}
