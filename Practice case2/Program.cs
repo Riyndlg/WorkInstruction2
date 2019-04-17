48e57538ea9babdbfc561f2bf47c055339026ede
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-17 19:51:32 +0300

    d

---
+++ Practice case2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 3 integer numbers:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if ((b > c && b < a) || (b < c && b > a))
+            {
+                Console.WriteLine("Число B находится между числами A и C");
+            }
+            else Console.WriteLine("Число B не находится между числами A и C");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practice case3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter integer number: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n % 2 != 0 && n > 99 && n < 1000)
+            {
+                Console.Write("True");
+            }
+            else Console.Write("False");
+            Console.ReadKey();
+        }
+    }
+}
