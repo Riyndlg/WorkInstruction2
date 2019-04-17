6f2d4e22ba0aaa92bd51c2e2edcc41431b42f608
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-17 20:22:52 +0300

    prrrr

---
+++ Practice case4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 12;
+            int z = 3;
+            x += y - x++ * z;
+            Console.WriteLine(x);
+            z = --x - y * 5;
+            Console.WriteLine(z);
+            y /= x + 5 % z;
+            Console.WriteLine(y);
+            z = x++ + y * 5;
+            Console.WriteLine(z);
+            x = y - x++ * z;
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
