a08dc8ecf06ed0caba0f305ff13c1c00af610e9a
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-18 18:35:39 +0300

    jj

---
+++ Practice case 7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter word about weather in Russian to translate: ");
+            string word = Console.ReadLine();
+            switch (word)
+            {
+                case "снег":
+                    Console.WriteLine("snow");
+                    break;
+                case "дождь":
+                    Console.WriteLine("rain");
+                    break;
+                case "ветер":
+                    Console.WriteLine("wind");
+                    break;
+                case "солнечно":
+                    Console.WriteLine("sunny");
+                    break;
+                case "гололед":
+                    Console.WriteLine("ice");
+                    break;
+                case "ливень":
+                    Console.WriteLine("shower");
+                    break;
+                case "облачно":
+                    Console.WriteLine("cloudy");
+                    break;
+                case "пасмурно":
+                    Console.WriteLine("mainly cloudy");
+                    break;
+                case "ветрено":
+                    Console.WriteLine("windy");
+                    break;
+                case "ясно":
+                    Console.WriteLine("clear");
+                    break;
+                default:
+                    Console.WriteLine("No translation avaivable");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
