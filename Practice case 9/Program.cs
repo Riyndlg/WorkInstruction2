464a4885eb2e7e231ad3f2e0e9e9bf97ee213371
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-18 19:25:16 +0300

    c

--- Practice case 9/Program.cs
+++ Practice case 9/Program.cs
@@ -10,7 +10,45 @@ namespace Practice_case_9
     {
         static void Main(string[] args)
         {
-
+            int operand1, operand2;
+            string sign;
+            string choice = "N";
+            while (choice == "N")
+            {
+                Console.WriteLine("Enter value for operands:");
+                operand1 = int.Parse(Console.ReadLine());
+                operand2 = int.Parse(Console.ReadLine());
+                Console.Write("Enter sign for operation: ");
+                sign = Console.ReadLine();
+                switch (sign)
+                {
+                    case "+":
+                        Console.WriteLine(operand1 + operand2);
+                        break;
+                    case "-":
+                        Console.WriteLine(operand1 - operand2);
+                        break;
+                    case "*":
+                        Console.WriteLine(operand1 * operand2);
+                        break;
+                    case "/":
+                        if ((operand1 != 0) && (operand2 != 0))
+                            Console.WriteLine(operand1 / operand2);
+                        else
+                        {
+                            Console.WriteLine("Operation was cancelled, error in value of operands. Do you want to try again? Y/N:");
+                            choice = Console.ReadLine();
+                            choice.ToUpper();
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Wrong value, this operation not exist. Do you want to try again? Y/N:");
+                        choice = Console.ReadLine();
+                        choice.ToUpper();
+                        break;
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
