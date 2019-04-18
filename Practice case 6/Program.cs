f2ee4f4697d97df4a4446090376b9088bfcfe581
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-18 18:16:21 +0300

    cder

---
+++ Practice case 6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter integer number from 0 to 100: ");
+            int number = int.Parse(Console.ReadLine());
+            if ((number >= 51) && (number <= 100))
+            {
+                Console.WriteLine("Number between 51-100");
+            }
+            else if ((number >= 15) && (number <= 35))
+            {
+                Console.WriteLine("Number between 15-35");
+            }
+            else if ((number >= 36) && (number <= 50))
+            {
+                Console.WriteLine("Number between 36-50");
+            }
+            else if ((number >= 0) && (number <= 14))
+            {
+                Console.WriteLine("Number between 0-14");
+            }
+            else Console.WriteLine("Number is out of range 0-100");
+            Console.ReadKey();
+        }
+    }
+}
