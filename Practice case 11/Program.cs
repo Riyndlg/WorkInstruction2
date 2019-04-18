9aee9fe8826997e46c8681c1c8a68eba6ecc3a31
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-18 19:45:45 +0300

    m

---
+++ Practice case 11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
