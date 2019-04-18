c62d3ab4162927de95ca7c6b323cbf0d531d7a8f
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-18 19:35:07 +0300

    v

---
+++ Practice case 10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter positive number:");
+            int number = int.Parse(Console.ReadLine());
+            int factorial = number;
+            if (number > 0)
+            {
+                for (int i = number; i > 1;)
+                {
+                    factorial = factorial * (i - 1);
+                    i--;
+                }
+                Console.WriteLine($"{number}!={factorial}");
+            }
+            else Console.WriteLine("You have enter wrong number");
+        }
+    }
+}
