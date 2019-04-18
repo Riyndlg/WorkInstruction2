ed17adfb83140a2f54d31d165695e208bac8c4f5
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-18 18:57:23 +0300

    bububy

---
+++ Practice case 8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_case_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter years of long service for employeer: ");
+            int service = int.Parse(Console.ReadLine());
+            if (service < 5)
+            {
+                Console.WriteLine("Premium 10% from salary");
+            }
+            if ((service < 10) && (service >=5))
+            {
+                Console.WriteLine("Premium 15% from salary");
+            }
+            if ((service < 15) && (service >= 10))
+            {
+                Console.WriteLine("Premium 25% from salary");
+            }
+            if ((service < 20) && (service >= 15))
+            {
+                Console.WriteLine("Premium 35% from salary");
+            }
+            if ((service < 25) && (service >= 20))
+            {
+                Console.WriteLine("Premium 45% from salary");
+            }
+            if (service >= 25)
+            {
+                Console.WriteLine("Premium 50% from salary");
+            }
+            Console.ReadKey();
+        }
+    }
+}
