5657972fd4c30fbbb051aaf921ba8dcb8d2cddfa
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-19 18:37:10 +0300

    finish

--- Practice case 11/Program.cs
+++ Practice case 11/Program.cs
@@ -10,17 +10,88 @@ namespace Practice_case_11
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
+            //строка
+            for (int i = 0; i < 15; i++)
             {
                 Console.Write("*");
             }
             Console.Write("\n");
+            //прямоугольник
             for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < 10; j++)
                 {
                     Console.Write("*");
                 }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+            //прямоугольный треугольник
+            int temp = 19;
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    if (j >= temp)
+                        {
+                            Console.Write(" ");
+                        }
+                    else Console.Write("*");
+                }
+                Console.Write("\n");
+                temp--;
+            }
+            Console.Write("\n");
+            //равносторонний треугольник
+            temp = 10;
+            int temp1 = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if ((j > temp1) && (j < temp))
+                        {
+                            Console.Write("*");
+                        }
+                    else Console.Write(" ");
+                }
+                Console.Write("\n");
+                temp--;
+                temp1++;
+            }
+            Console.WriteLine();
+            //ромб
+            temp = 10;
+            temp1 = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if ((j < temp1) && (j > temp))
+                        {
+                            Console.Write("*");
+                        }
+                    else Console.Write(" ");
+                }
+                Console.Write("\n");
+                temp--;
+                temp1++;
+            }
+            temp = 10;
+            temp1 = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if ((j > temp1) && (j < temp))
+                        {
+                            Console.Write("*");
+                        }
+                    else Console.Write(" ");
+                }
+                Console.Write("\n");
+                temp--;
+                temp1++;
             }
             Console.ReadKey();
         }
